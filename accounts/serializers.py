2759cc9b232c6f81c986555f8fc1739f7dddde84
Abu-Hafsah <najib.mahmud@student.moringaschool.com>
2026-07-22 23:30:46 +0300

    updated serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,18 +7,27 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["id", "username", "email", "password"]
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+        ]
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        return User.objects.create_user(
             username=validated_data["username"],
             email=validated_data["email"],
             password=validated_data["password"],
         )
-        return user
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email"]
+        fields = [
+            "id",
+            "username",
+            "email",
+            "is_staff",
+        ]
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from .views import RegisterView, CurrentUserView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+
     path("login/", TokenObtainPairView.as_view(), name="login"),
+
+    path(
+        "login/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
+
     path("me/", CurrentUserView.as_view(), name="current-user"),
 ]
